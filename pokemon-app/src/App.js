81e7443b2f841285de3e109a8a31851b30bc46ae
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-12-04 11:03:22 +0900

    feature:ポケモンデータの取得

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
+  // ポケモンAPIのURL
+  const initialURL = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
   return <div className="App">hello</div>;
 }
 
