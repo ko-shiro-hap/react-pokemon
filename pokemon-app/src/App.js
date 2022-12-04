ad7df2426ce0b2fd99d9fa3af3590489633f882b
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-12-04 13:21:38 +0900

    feature:ポケモンの詳細なデータを取得

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,28 +1,44 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllPokemon } from "./utils/pokemon";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 function App() {
   // ポケモンAPIのURL
-  const initialURL = "https://pokeapi.co/api/v2/pokemon/ditto";
+  const initialURL = "https://pokeapi.co/api/v2/pokemon/";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
   return (
-    <>
-      <div className="App">
-        {loading ? <h1>ロード中...</h1> : <h1>ポケモンデータを取得しました</h1>}
-      </div>
-    </>
+    <div className="App">
+      {loading ? (
+        <h1>ロード中...</h1>
+      ) : (
+        <div className="pokemonCardContainer"></div>
+      )}
+    </div>
   );
 }
 
